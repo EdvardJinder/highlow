40570a6fc78ed60aa2c6d0e6d3cd41f37c5afe9a
EdvardJinder <89024464+EdvardJinder@users.noreply.github.com>
2022-08-30 12:03:32 +0200

    encuragement

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import axios from "axios";
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 const dark = ref(true);
 const otherGames = ref(false);
+const insult = ref(null);
+const alert = ref(null);
 
 const game = ref(null);
 axios
@@ -29,6 +31,7 @@ const guess = async (higher) => {
 const restart = async () => {
   const response = await axios.get(`https://localhost:7253/api/Game/Start`);
   game.value = response.data;
+  insult.value = null;
 };
 
 const computedCardUrl = computed(() => {
@@ -56,6 +59,27 @@ const computedCardUrl = computed(() => {
 
   return `${color}_${game.value.currentCard.value}`;
 });
+
+watch(game, (value) => {
+  if (!value.isRunning) {
+    axios.get("https://insult.mattbas.org/api/insult").then((response) => {
+      insult.value = response.data;
+      console.log(insult.value);
+    });
+  } else {
+    if (value.score % 5 == 0 && value.score > 4) {
+      console.log(value.score);
+      alert.value = {
+        type: "success",
+        message: "You are doing great!",
+      };
+
+      setTimeout(() => {
+        alert.value = null;
+      }, 3000);
+    }
+  }
+});
 </script>
 
 <template>
@@ -68,31 +92,30 @@ const computedCardUrl = computed(() => {
       <v-btn @click="otherGames = !otherGames">Other games</v-btn>
     </v-app-bar>
     <v-main>
+      <v-alert v-if="alert" :title="alert.message" :type="alert.type"></v-alert>
       <v-container
         v-if="!otherGames"
         class="d-flex w-100 h-100 align-center justify-center text-center"
       >
-        <div v-if="game && game.deck.count > 0">
-          <h3 class="mb-5">SCORE: {{ game.score }}</h3>
-
+        <div v-if="game && game.deck.count > 0" class="d-flex flex-column align-center">
           <!-- <h2>{{ game.currentCard.value }}</h2> -->
-          <v-img :src="`src/assets/cards/${computedCardUrl}.svg`" size="260" />
+          <h3 class="mb-5">SCORE: {{ game.score }}</h3>
+          <v-img :src="`src/assets/cards/${computedCardUrl}.svg`" width="250" />
           <div v-if="game.isRunning">
             <div class="my-5">
-              <v-btn @click="guess(false)">Lower</v-btn>
-              <v-btn class="mx-1" @click="guess(null)">Same</v-btn>
-              <v-btn @click="guess(true)">Higher</v-btn>
+              <v-btn @click="guess(false)" color="red">Lower</v-btn>
+              <v-btn class="mx-1" @click="guess(null)" color="yellow">Same</v-btn>
+              <v-btn @click="guess(true)" color="blue">Higher</v-btn>
             </div>
+            <h4>Cards left: {{ game.deck.count }}</h4>
           </div>
-          <div v-else>
-            <v-btn class="mr-3 my-3" @click="restart">Restart</v-btn>
-            <v-btn @click="highscore">View Highscore</v-btn>
-            <h2 class="mt-3">FUCK YOU LOSER!</h2>
+          <div v-else class="mt-3">
+            <h2 class="mt-3" v-if="insult">{{ insult }}</h2>
+            <v-btn class="mr-3 my-3" @click="restart" color="blue">Restart</v-btn>
+            <v-btn @click="highscore" color="yellow">View Highscore</v-btn>
           </div>
-
-          <h4>Cards left: {{ game.deck.count }}</h4>
         </div>
-        <div v-else>FUCK OFF</div>
+        <div v-else>Something went wrong...</div>
       </v-container>
       <v-container v-else> Ricky </v-container>
     </v-main>
